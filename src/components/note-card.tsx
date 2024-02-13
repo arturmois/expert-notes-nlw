40bee02b73091f7a80c8666965f3cbca99d4d913
Artur Moisés <arturmoiscontato@gmail.com>
2024-02-13 19:09:43 -0300

    feat: add delete note function

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,12 +5,14 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
+    id: string
     date: Date
     content: string
   }
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -40,6 +42,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
           <button
             type="button"
+            onClick={() => onNoteDeleted(note.id)}
             className="group w-full bg-slate-800 py-4 text-center text-sm font-medium text-slate-300 outline-none"
           >
             Deseja{' '}
